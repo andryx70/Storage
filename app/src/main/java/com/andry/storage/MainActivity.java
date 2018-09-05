099684cd5716b008298a5ee25ccb88cbebec1dd3
Andry <andry.jose.v@gmail.com>
2018-09-04 21:47:06 -0400

    2 commit

--- app/src/main/java/com/andry/storage/MainActivity.java
+++ app/src/main/java/com/andry/storage/MainActivity.java
@@ -1,13 +1,128 @@
 package com.andry.storage;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int CHOOSER_IMAGES = 1;
+    private static final String TAG = "MainActivity";
+    private Button btnDownload, btnUpload;
+    private ImageView imvPicture;
+
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        btnDownload = findViewById(R.id.btn_download);
+        btnUpload = findViewById(R.id.btn_upload);
+        imvPicture = findViewById(R.id.imv_picture);
+
+        imvPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Selecciona una imagen"), CHOOSER_IMAGES);
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StorageReference pruebaRef = storageReference.child("prueba.png");
+                imvPicture.setDrawingCacheEnabled(true);
+                imvPicture.buildDrawingCache();
+
+                Bitmap bitmap = imvPicture.getDrawingCache();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+
+                byte[] pruebaByte = baos.toByteArray();
+
+                UploadTask uploadTask = pruebaRef.putBytes(pruebaByte);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.e(TAG, "Ocurrio un error");
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(MainActivity.this, "Subida realizada", Toast.LENGTH_SHORT).show();
+                        String downloadUri = taskSnapshot.getUploadSessionUri().getPath();
+
+                    }
+                });
+            }
+        });
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final File file;
+                try {
+                    file = File.createTempFile("Boceto", "png");
+                    storageReference.child("Boceto.png").getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+                            imvPicture.setImageBitmap(bitmap);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            
+                        }
+                    });
+                }catch (Exception e){
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Ocurrio un error en la descarga", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CHOOSER_IMAGES){
+            Uri imageUri = data.getData();
+            if (imageUri != null){
+                imvPicture.setImageURI(imageUri);
+            }
+        }
     }
 }
